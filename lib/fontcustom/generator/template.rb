ed4518dd38bd6ffbd240ba11e0a54c36ca4c4019
Juan Manuel Docile <jmdocile@gmail.com>
2022-09-30 11:13:13 -0300

    fix nombre de etiqueta

--- lib/fontcustom/generator/template.rb
+++ lib/fontcustom/generator/template.rb
@@ -208,7 +208,7 @@ module Fontcustom
   text-transform: none;
   -moz-osx-font-smoothing: grayscale;
   -webkit-font-smoothing: antialiased;
-  font-smoothing: auto; |
+  font-smooth: auto; |
       end
 
       def glyphs
